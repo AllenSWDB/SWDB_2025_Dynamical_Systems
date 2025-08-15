cf17d85e09b3a144358dbdac68caf281c7d73a93
Yusi Chen <chenyusi151201@gmail.com>
2025-08-15 02:25:42 -0700

    changes to variable names and fixed typos

--- code/utils_model_recovery.py
+++ code/utils_model_recovery.py
@@ -39,6 +39,54 @@ logger.addHandler(console_handler)
 # -----------
 
 
+def get_nwb_from_local_tmp(session_id):
+    """Get NWB file from session_id.
+
+    Overwrite this function to get NWB file from other places.
+
+    Parameters
+    ----------
+    session_id : _type_
+        _description_
+    """
+    io = NWBHDF5IO(f"{LOCAL_NWB_TMP}/{session_id}.nwb", mode="r")
+    nwb = io.read()
+    return nwb
+
+
+def get_history_from_nwb(nwb):
+    """Get choice and reward history from nwb file
+    
+    #TODO move this to aind-behavior-nwb-util
+    """
+
+    df_trial = nwb.trials.to_dataframe()
+
+    autowater_offered = (df_trial.auto_waterL == 1) | (df_trial.auto_waterR == 1)
+    choice_history = df_trial.animal_response.map({0: 0, 1: 1, 2: np.nan}).values
+    reward_history = df_trial.rewarded_historyL | df_trial.rewarded_historyR
+    p_reward = [
+        df_trial.reward_probabilityL.values,
+        df_trial.reward_probabilityR.values,
+    ]
+    random_number = [
+        df_trial.reward_random_number_left.values,
+        df_trial.reward_random_number_right.values,
+    ]
+
+    baiting = False if "without baiting" in nwb.protocol.lower() else True
+
+    return (
+        baiting,
+        choice_history,
+        reward_history,
+        p_reward,
+        autowater_offered,
+        random_number,
+    )
+
+
+
 class DynamicForagingAgentMLEBase(DynamicForagingAgentBase):
     """Base class of "DynamicForagingAgentBase" + "MLE fitting" """
 
